c658c4b473ef2e7bafcd427c0d51d84aa7ec4f0a
Dan Esparza <esparza.dan@gmail.com>
2016-05-18 14:39:24 -0400

    Updated client error handling.  Added remove, getall and associated unit tests

--- client.go
+++ client.go
@@ -59,7 +59,47 @@ func (client ConfigClient) Get(name string) (ConfigResponse, error) {
 
 	//	Post the JSON to the api url
 	res, err := http.Post(apiUrl, "application/json", requestJSON)
-	defer res.Body.Close()
+	if res != nil {
+		defer res.Body.Close()
+	}
+
+	if err != nil {
+		return retval, err
+	}
+
+	//	Decode the return object
+	err = json.NewDecoder(res.Body).Decode(&retval)
+	if err != nil {
+		return retval, err
+	}
+
+	return retval, nil
+}
+
+//	Get a specific config item
+func (client ConfigClient) GetAll() (ConfigResponseMultiple, error) {
+	retval := ConfigResponseMultiple{}
+
+	//	First, see if we have a service url set:
+	if client.ServiceUrl == "" {
+		err := fmt.Errorf("Please specify a 'ServiceUrl' for the centralconfig service")
+		return retval, err
+	}
+
+	apiUrl := client.ServiceUrl + "/config/getall"
+
+	// Convert bytes to a reader.
+	requestJSON := strings.NewReader("")
+
+	//	Post the JSON to the api url
+	res, err := http.Post(apiUrl, "application/json", requestJSON)
+	if res != nil {
+		defer res.Body.Close()
+	}
+
+	if err != nil {
+		return retval, err
+	}
 
 	//	Decode the return object
 	err = json.NewDecoder(res.Body).Decode(&retval)
@@ -100,7 +140,13 @@ func (client ConfigClient) Set(request *ConfigItem) (ConfigResponse, error) {
 
 	//	Post the JSON to the api url
 	res, err := http.Post(apiUrl, "application/json", requestJSON)
-	defer res.Body.Close()
+	if res != nil {
+		defer res.Body.Close()
+	}
+
+	if err != nil {
+		return retval, err
+	}
 
 	//	Decode the return object
 	err = json.NewDecoder(res.Body).Decode(&retval)
@@ -109,4 +155,57 @@ func (client ConfigClient) Set(request *ConfigItem) (ConfigResponse, error) {
 	}
 
 	return retval, nil
+}
+
+//	Remove removes a config item
+func (client ConfigClient) Remove(request *ConfigItem) error {
+	retval := ConfigResponse{}
+
+	//	First, see if we have a service url set:
+	if client.ServiceUrl == "" {
+		err := fmt.Errorf("Please specify a 'ServiceUrl' for the centralconfig service")
+		return err
+	}
+
+	apiUrl := client.ServiceUrl + "/config/remove"
+
+	//	Check to see if we have an application
+	if request.Application == "" {
+		err := fmt.Errorf("Please specify an 'Application' to remove configuration for")
+		return err
+	}
+
+	//	Check to see if we have a name
+	if request.Name == "" {
+		err := fmt.Errorf("Please specify a 'Name' to remove configuration for")
+		return err
+	}
+
+	//	Serialize our request to JSON:
+	requestBytes := new(bytes.Buffer)
+	err := json.NewEncoder(requestBytes).Encode(request)
+	if err != nil {
+		return err
+	}
+
+	// Convert bytes to a reader.
+	requestJSON := strings.NewReader(requestBytes.String())
+
+	//	Post the JSON to the api url
+	res, err := http.Post(apiUrl, "application/json", requestJSON)
+	if res != nil {
+		defer res.Body.Close()
+	}
+
+	if err != nil {
+		return err
+	}
+
+	//	Decode the return object
+	err = json.NewDecoder(res.Body).Decode(&retval)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- client_test.go
+++ client_test.go
@@ -27,7 +27,9 @@ func TestConfigManager_Get_ReturnsConfigResponse(t *testing.T) {
 	//	Arrange
 	config := getCentralConfigInfo()
 	expected := ""
-	t.Logf("Using config client information: %v", config)
+	if config.ServiceUrl == "" {
+		t.Fatalf("Oops!  It looks like you need to set the 'centralconfig_service_url' environment variable to the service endpoint of the centralconfig service.")
+	}
 
 	//	Act
 	response, err := config.Get("TestItem42")
@@ -61,10 +63,11 @@ func TestConfigManager_Set_ReturnsConfigResponse(t *testing.T) {
 
 	//	Act
 	response, err := config.Set(&request)
+	defer config.Remove(&request)
 
 	//	Assert
 	if err != nil {
-		t.Errorf("Can't get config: %v", err)
+		t.Errorf("Can't set config: %v", err)
 	}
 
 	if response.Status != http.StatusOK {
@@ -92,16 +95,17 @@ func TestConfigManager_Set_WithMachine_ReturnsConfigResponse(t *testing.T) {
 		Application: unitTestAppName,
 		Machine:     hostName,
 		Name:        "TestItem42",
-		Value:       "SetFirstTime"}
+		Value:       "Set_WithMachine"}
 
-	expected := "SetFirstTime"
+	expected := "Set_WithMachine"
 
 	//	Act
 	response, err := config.Set(&request)
+	defer config.Remove(&request)
 
 	//	Assert
 	if err != nil {
-		t.Errorf("Can't get config: %v", err)
+		t.Errorf("Can't set config: %v", err)
 	}
 
 	if response.Status != http.StatusOK {
@@ -114,5 +118,122 @@ func TestConfigManager_Set_WithMachine_ReturnsConfigResponse(t *testing.T) {
 
 	if response.Data.Machine != hostName {
 		t.Errorf("Config machine '%v' ins't expected.  Should be '%v'", response.Data.Machine, hostName)
+	}
+}
+
+//	We should be able to set and remove an item
+func TestConfigManager_Remove_Successful(t *testing.T) {
+	//	Arrange
+	config := getCentralConfigInfo()
+
+	request := configmanager.ConfigItem{
+		Application: unitTestAppName,
+		Name:        "TestItem42",
+		Value:       "Remove"}
+
+	//	Act
+	config.Set(&request)
+	err := config.Remove(&request)
+
+	//	Assert
+	if err != nil {
+		t.Errorf("Can't remove config: %v", err)
+	}
+}
+
+//	We should be able to set and remove an item
+func TestConfigManager_RemoveWithMachine_Successful(t *testing.T) {
+	//	Arrange
+	config := getCentralConfigInfo()
+
+	//	Set the hostname to the os Hostname or
+	//	the default hostname
+	hostName, err := os.Hostname()
+	if err != nil {
+		hostName = defaultMachineName
+	}
+
+	requests := []configmanager.ConfigItem{}
+
+	//	--- Config item without a machine name
+	requests = append(requests, configmanager.ConfigItem{
+		Application: unitTestAppName,
+		Name:        "TestItem42",
+		Value:       "RemoveWithMachine"})
+
+	//	--- Config item with machine name
+	requests = append(requests, configmanager.ConfigItem{
+		Application: unitTestAppName,
+		Machine:     hostName,
+		Name:        "TestItem42",
+		Value:       "RemoveWithMachine"})
+
+	//	Act
+	for _, item := range requests {
+		config.Set(&item)
+	}
+
+	for _, item := range requests {
+		err = config.Remove(&item)
+
+		//	Assert
+		if err != nil {
+			t.Errorf("Can't remove config: %v", err)
+		}
+	}
+}
+
+//	We should be able to set multiple items and get them all back
+func TestConfigManager_GetAll_ReturnsConfigResponseMultiple(t *testing.T) {
+	//	Arrange
+	config := getCentralConfigInfo()
+
+	//	Set the hostname to the os Hostname or
+	//	the default hostname
+	hostName, err := os.Hostname()
+	if err != nil {
+		hostName = defaultMachineName
+	}
+
+	requests := []configmanager.ConfigItem{}
+
+	//	--- Config item without a machine name
+	requests = append(requests, configmanager.ConfigItem{
+		Application: unitTestAppName,
+		Name:        "TestItem42",
+		Value:       "GetAll"})
+
+	//	--- Config item with machine name
+	requests = append(requests, configmanager.ConfigItem{
+		Application: unitTestAppName,
+		Machine:     hostName,
+		Name:        "TestItem42",
+		Value:       "GetAll"})
+
+	//	Clean up:
+	defer func() {
+		for _, item := range requests {
+			config.Remove(&item)
+		}
+	}()
+
+	//	Act
+	for _, item := range requests {
+		config.Set(&item)
+	}
+
+	response, err := config.GetAll()
+
+	//	Assert
+	if err != nil {
+		t.Errorf("Can't get all configs: %v", err)
+	}
+
+	if response.Status != http.StatusOK {
+		t.Errorf("ConfigResponse isn't OK: %v Response is %v:%v", err, response.Status, response.Message)
+	}
+
+	if len(response.Data) == 0 {
+		t.Errorf("Got '%v' items back.  Should be at least '%v' items", len(response.Data), len(requests))
 	}
 }
